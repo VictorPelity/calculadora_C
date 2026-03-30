6c7723af274f3896e425652a5937fcac8d25c01d
VictorPelity <victorhugopelity@gmail.com>
2026-03-30 15:35:08 -0300

    Código

---
+++ main.c
@@ -0,0 +1,130 @@
+/*Calculadora científica no terminal
+
+operações básicas + potência + raiz + fatorial*/
+
+#include <stdio.h>
+
+float soma(float x, float y){
+    return x+y;
+}
+
+float multiplicacao(float x, float y){
+    return x*y;
+}
+
+float divisao(float x, float y){
+    if(y == 0) return 0;
+    
+    return x/y;
+}
+
+float subtracao(float x, float y){
+    return x-y;
+}
+
+float potencia(float numero, float potencia){
+    int resultado = 1;
+    for(int i = 1; i<=potencia; i++){
+        resultado = resultado*numero;
+    }
+    return resultado;
+}
+
+float fatorial(float x){
+    
+    if(x==1) return 1;
+    else return x*(fatorial(x-1)); 
+}
+
+float raizQuadrada(float x){
+ 
+ int raiz, metade = x/2;
+ for(int i = 0; i<=metade; i++){
+     raiz = i*i;
+     if(raiz == x) return i;
+     else if(i == metade) return -1;
+ }
+}
+
+void clear_screen() {
+    // Sequência ANSI para mover o cursor para a posição 1,1 (topo esquerdo) e limpar a tela
+    printf("\e[1;1H\e[2J");
+    // Para garantir que a saída seja imediata
+    fflush(stdout);
+}
+
+
+
+int main()
+{
+    int opcao;
+    float num1, num2;
+    printf("Bem vindo ao calculaTogs!\n");
+    printf("Aqui temos 7 opções de cálculos:\n");
+    printf("-0 = Sair\n");
+    printf("-1 = Somar\n");
+    printf("-2 = Subtrair\n");
+    printf("-3 = Multiplicar\n");
+    printf("-4 = Dividir\n");
+    printf("-5 = Potencia\n");
+    printf("-6 = Raiz Quadrada\n");
+    printf("-7 = Fatorial\n");
+    printf("Qual deseja escolher: ");
+    scanf("%d", &opcao);
+    
+    clear_screen();
+        switch(opcao){
+        
+        default:
+        break;
+        
+        
+        case 1:
+        printf("Voce escolheu SOMAR, digite dois numeros: ");
+        scanf("%f %f", &num1, &num2);
+        printf("O resultado e = %.2f\n", soma(num1,num2));
+        break;
+        
+        case 2:
+        printf("Voce escolheu SUBTRAIR, digite dois numeros: ");
+        scanf("%f %f", &num1, &num2);
+        printf("O resultado e = %.2f\n", subtracao(num1,num2));
+        break;
+        
+        
+        case 3:
+        printf("Voce escolheu MULTIPLICAR, digite dois numeros: ");
+        scanf("%f %f", &num1, &num2);
+        printf("O resultado e = %.2f\n", multiplicacao(num1,num2));
+        break;
+        
+        
+        case 4:
+        printf("Voce escolheu DIVIDIR, digite dois numeros: ");
+        scanf("%f %f", &num1, &num2);
+        printf("O resultado e = %.2f\n", divisao(num1,num2));
+        break;
+        
+        
+        case 5:
+        printf("Voce escolheu POTENCIA, digite primeiro o numero e depois sua potencia: ");
+        scanf("%f %f", &num1, &num2);
+        printf("O resultado e = %.2f\n", potencia(num1,num2));
+        break;
+        
+        case 6:
+        printf("Voce escolheu RAIZ QUADRADA, digite o numero que quer achar a raiz: ");
+        scanf("%f", &num1);
+        if(raizQuadrada(num1) == -1) printf("Este número não tem raiz quadrada inteira.");
+        else printf("O resultado e = %.2f\n", raizQuadrada(num1));
+        break;
+        
+        case 7:
+        printf("Voce escolheu FATORIAL, digite um numero: ");
+        scanf("%f", &num1);
+        printf("O resultado e = %.2f\n", fatorial(num1));
+    }
+
+
+    return 0;
+}
