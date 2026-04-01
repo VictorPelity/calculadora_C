ee346f24350dd9f43ed6448b5ed0f24aa27efe0e
VictorPelity <victorhugopelity@gmail.com>
2026-04-01 17:14:49 -0300

    Refactor mathematical functions and main logic

--- main.c
+++ main.c
@@ -13,7 +13,6 @@ float multiplicacao(float x, float y){
 }
 
 float divisao(float x, float y){
-    if(y == 0) return 0;
     
     return x/y;
 }
@@ -22,7 +21,7 @@ float subtracao(float x, float y){
     return x-y;
 }
 
-float potencia(float numero, float potencia){
+int potencia(int numero, int potencia){
     int resultado = 1;
     for(int i = 1; i<=potencia; i++){
         resultado = resultado*numero;
@@ -30,20 +29,27 @@ float potencia(float numero, float potencia){
     return resultado;
 }
 
-float fatorial(float x){
-    
-    if(x==1) return 1;
-    else return x*(fatorial(x-1)); 
+int fatorial(int numero){
+    if(numero==0) return 1;
+    else return numero*(fatorial(numero-1)); 
 }
 
-float raizQuadrada(float x){
- 
- int raiz, metade = x/2;
- for(int i = 0; i<=metade; i++){
+int raizQuadrada(int numero){
+ int raiz = -1;
+ for(int i = 0; i<=numero; i++){
      raiz = i*i;
-     if(raiz == x) return i;
-     else if(i == metade) return -1;
+     if(raiz == numero) return i;
  }
+ return raiz;
+}
+
+int raizCubica(int numero){
+ int raiz = -1;
+ for(int i = 0; i<=numero; i++){
+     raiz = i*i*i;
+     if(raiz == numero) return i;
+ }
+ return raiz;
 }
 
 void clear_screen() {
@@ -53,26 +59,26 @@ void clear_screen() {
     fflush(stdout);
 }
 
-
-
-int main()
-{
-    int opcao;
+void calculos(){
+    int opcao, continuar;
     float num1, num2;
-    printf("Bem vindo ao calculaTogs!\n");
-    printf("Aqui temos 7 opções de cálculos:\n");
-    printf("-0 = Sair\n");
+    int numero1,numero2;
+    do {
+    printf("\n-0 = Sair\n");
     printf("-1 = Somar\n");
     printf("-2 = Subtrair\n");
     printf("-3 = Multiplicar\n");
     printf("-4 = Dividir\n");
     printf("-5 = Potencia\n");
     printf("-6 = Raiz Quadrada\n");
-    printf("-7 = Fatorial\n");
-    printf("Qual deseja escolher: ");
+    printf("-7 = Raiz Cúbica\n");
+    printf("-8 = Fatorial\n");
+    printf("\nQual deseja escolher: ");
     scanf("%d", &opcao);
-    
     clear_screen();
+        
+        
+        
         switch(opcao){
         
         default:
@@ -83,48 +89,71 @@ int main()
         printf("Voce escolheu SOMAR, digite dois numeros: ");
         scanf("%f %f", &num1, &num2);
         printf("O resultado e = %.2f\n", soma(num1,num2));
+         
         break;
         
         case 2:
         printf("Voce escolheu SUBTRAIR, digite dois numeros: ");
         scanf("%f %f", &num1, &num2);
         printf("O resultado e = %.2f\n", subtracao(num1,num2));
+         
         break;
-        
         
         case 3:
         printf("Voce escolheu MULTIPLICAR, digite dois numeros: ");
         scanf("%f %f", &num1, &num2);
         printf("O resultado e = %.2f\n", multiplicacao(num1,num2));
+         
         break;
-        
         
         case 4:
         printf("Voce escolheu DIVIDIR, digite dois numeros: ");
         scanf("%f %f", &num1, &num2);
-        printf("O resultado e = %.2f\n", divisao(num1,num2));
-        break;
+        if(num2 == 0) printf("Não é possível dividir nenhum número por zero.\n");
+        else printf("O resultado e = %.2f\n", divisao(num1,num2));
         
+        break;
         
         case 5:
         printf("Voce escolheu POTENCIA, digite primeiro o numero e depois sua potencia: ");
-        scanf("%f %f", &num1, &num2);
-        printf("O resultado e = %.2f\n", potencia(num1,num2));
+        scanf("%d %d", &numero1, &numero2);
+        printf("O resultado e = %d\n", potencia(numero1,numero2));
+         
         break;
         
         case 6:
         printf("Voce escolheu RAIZ QUADRADA, digite o numero que quer achar a raiz: ");
-        scanf("%f", &num1);
-        if(raizQuadrada(num1) == -1) printf("Este número não tem raiz quadrada inteira.");
-        else printf("O resultado e = %.2f\n", raizQuadrada(num1));
+        scanf("%d", &numero1);
+        if(raizQuadrada(numero1) == -1) printf("Este número não tem raiz quadrada exata.");
+        else printf("O resultado e = %d\n", raizQuadrada(numero1));
+         
         break;
         
         case 7:
+        printf("Voce escolheu RAIZ CÚBICA, digite o numero que quer achar a raiz: ");
+        scanf("%d", &numero1);
+        if(raizCubica(numero1) == -1) printf("Este número não tem raiz cúbica exata.");
+        else printf("O resultado e = %d\n", raizCubica(numero1));
+         
+        break;
+        
+        
+        case 8:
         printf("Voce escolheu FATORIAL, digite um numero: ");
-        scanf("%f", &num1);
-        printf("O resultado e = %.2f\n", fatorial(num1));
+        scanf("%d", &numero1);
+        if(numero1 <0){
+            printf("Não existe fatorial de número negativo.\n");
+        }
+        else printf("O resultado e = %d\n", fatorial(numero1));
+         
+    }  
     }
+    while (opcao >=1 && opcao <9);
+}
 
 
+int main()
+{
+    calculos();
     return 0;
 }
